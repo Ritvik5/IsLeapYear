9ba1c578e253e6c98ef711ad15fd54574e1739a3
Ritvik5 <sharma.ritvik56@gmail.com>
2023-02-22 00:51:46 +0530

    Taking Input and Checking LeapYear

---
+++ LeapYear/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Year: ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            if (year>=1000 && year<=9999)
+            {
+                Console.WriteLine("The Given Input is Correct");
+            }
+            else
+            {
+                Console.WriteLine("Enter Valid year");
+            }
+
+            if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+            {
+                Console.WriteLine(year+ " is a Leap Year");
+            }
+            else
+            {
+                Console.WriteLine(year+ " is not a Leap Year");
+            }
+        }
+    }
+}
